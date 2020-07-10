71a7c35db91faee4a92aed30d0b9bf2c93615b6f
jayesh1044 <jayeshpatil1044@gmail.com>
2020-07-09 19:19:57 -0700

    first commit

---
+++ src/main/java/com/example/controller/HomeResource.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String home()
+	{
+		
+		return ("<h1>Welcome home</h1>");
+	}
+	
+	@GetMapping("/user")
+	public String user()
+	{
+		return ("<h1>Welcome User</h1>");
+	}
+	
+	@GetMapping("/admin")
+	public String admin()
+	{
+		return ("<h1>Welcome Admin</h1>");
+		
+	}
+	
+
+}
